8238408795be611afb97673601f126c66ebe27e0
Anders Petershagen Åsbø <aanders35@gmail.com>
2020-05-15 22:58:54 +0200

    added leapfrog integration.

--- src/integrate.c
+++ src/integrate.c
@@ -1,6 +1,30 @@
 
-int integrate(int T, double dt, int N, double x0, double y0, double vx0,
-              double vy0, const double G, const int sun_mass) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "lib.h"
 
+double Ga(double X, double Y, const double G, const int sun_mass);
+
+int integrate(int T, double dt, int N, double X[], double Y[], double Vx[], double Vy[], const double G, const int sun_mass) {
+  double a1, a2, aa, dta1;
+
+  for (size_t i = 0; i < N-1; i++) {
+    a1 = Ga(X[i], Y[i], G, sun_mass)*dt*0.5;
+    dta1 = a1*dt;
+    X[i+1] = X[i] + dt*Vx[i] + dta1;
+    Y[i+1] = Y[i] + dt*Vy[i] + dta1;
+
+    a2 = Ga(X[i+1], Y[i+1], G, sun_mass)*dt*0.5;
+    aa = a1+a2;
+    Vx[i+1] = Vx[i] + aa;
+    Vy[i+1] = Vy[i] + aa;
+
+  }
   return 0;
+}
+
+double Ga(double X, double Y, const double G, const int sun_mass) {
+  double r2 = X*X + Y*Y;
+  return -G*sun_mass/r2;
 }
--- src/main.c
+++ src/main.c
@@ -9,15 +9,27 @@ int main(int argc, char const *argv[]) {
   int T = atoi(argv[2]);
 
   const double G = 4*M_PI*M_PI;
+  const int sun_mass = 1;
+  double dt = (double) T / N;
+
   double x0 = 1.00000102;
   double y0 = 0.0;
   double vx0 = 0.0;
   double vy0 = 6.28;
-  const int sun_mass = 1;
-  double dt = (double) T / N;
+
+  double X[N];
+  double Y[N];
+  double Vx[N];
+  double Vy[N];
+
   printf("%lf %lf %lf %lf %lf %lf\n", G, x0, y0, vx0, vy0, dt);
 
-  integrate(T, dt, N, x0, y0, vx0, vy0, G, sun_mass);
+  X[0] = x0;
+  Y[0] = y0;
+  Vx[0] = vx0;
+  Vy[0] = vy0;
+
+  integrate(T, dt, N, X, Y, Vx, Vy, G, sun_mass);
 
   return 0;
 }
