1b473b7fbecda2c9324844710d119b642fc7f379
Anders Petershagen Åsbø <aanders35@gmail.com>
2020-05-15 23:33:57 +0200

    changed memory allocation.

--- src/lib.h
+++ src/lib.h
@@ -2,7 +2,7 @@
 #define LIB_H
 
 // in integrate.c:
-int integrate(int T, double dt, int N, double X[], double Y[], double Vx[], double Vy[], const double G, const int sun_mass);
+int integrate(int T, double dt, int N, double **X, double **Y, double **Vx, double **Vy, const double G, const int sun_mass);
 
 // in file extrafunc.c:
 int alloc2D(double ***A, int n, int m);
--- src/main.c
+++ src/main.c
@@ -6,21 +6,21 @@
 
 int main(int argc, char const *argv[]) {
   int N = atoi(argv[1]);
-  int T = atoi(argv[2]);
+  double T = atoi(argv[2]);
 
   const double G = 4*M_PI*M_PI;
   const int sun_mass = 1;
-  double dt = (double) T / N;
+  double dt = T / N;
 
   double x0 = 1.00000102;
   double y0 = 0.0;
   double vx0 = 0.0;
   double vy0 = 6.28;
 
-  double X[N];
-  double Y[N];
-  double Vx[N];
-  double Vy[N];
+  double *X = malloc(N*sizeof(X));
+  double *Y = malloc(N*sizeof(Y));
+  double *Vx = malloc(N*sizeof(Vx));
+  double *Vy = malloc(N*sizeof(Vy));
 
   printf("%lf %lf %lf %lf %lf %lf\n", G, x0, y0, vx0, vy0, dt);
 
@@ -29,7 +29,16 @@ int main(int argc, char const *argv[]) {
   Vx[0] = vx0;
   Vy[0] = vy0;
 
-  integrate(T, dt, N, X, Y, Vx, Vy, G, sun_mass);
+  integrate(T, dt, N, &X, &Y, &Vx, &Vy, G, sun_mass);
+  for (size_t i = 0; i < 4; i++) {
+    printf("(%lf,%lf)\n", X[i],Y[i]);
+    printf("[%lf,%lf]\n", Vx[i],Vy[i]);
+  }
+
+  free(X);
+  free(Y);
+  free(Vx);
+  free(Vy);
 
   return 0;
 }
